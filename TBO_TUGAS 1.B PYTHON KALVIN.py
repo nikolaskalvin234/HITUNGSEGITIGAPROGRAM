81a5352c1a76a30fb445e2cfefc5c407136e8b3f
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 18:18:44 +0700

    first commit

---
+++ TBO_TUGAS 1.B PYTHON KALVIN.py
@@ -0,0 +1,16 @@
+#.CODING PYTHON - [ TUGAS - 1.B ]
+#.KULIAH TEORI BAHASA & OTOMATA
+#.DOSEN = Arnes Y.V, M.Kom
+
+print("NAMA = NIKOLAS KALVIN")
+print("NPM = 21421002")
+print('')
+# Menginput Alas dan Tinggi Segitiga
+alas = float(input('Tulis Alas Segitiga: '))
+tinggi = float(input('Tulis Tinggi Segitiga: '))
+
+# Hitung Luas Segitiga
+luas = (alas * tinggi) / 2
+
+# Menampilkan Hasil Perhitungan
+print('Luas Segitiga adalah %0.2f' %luas)
